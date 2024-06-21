f55c480fab22eb01e989334471295b710d5f881e
quynhuw <qinhuuuuu@gmail.com>
2024-06-21 13:33:18 +0700

    fix deploy

--- src/components/CategoryBar/index.tsx
+++ src/components/CategoryBar/index.tsx
@@ -1,29 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const CategoryBar = () => {
+  const navigate = useNavigate();
   return (
     <div className=" bg-primary">
       <div className="flex items-center justify-center py-2 mx-auto text-[19px] font-semibold text-white max-w-7xl gap-x-16">
-        <NavLink to="/">
+        <div onClick={() => navigate("/")}>
           <div className="cursor-pointer category hover:text-tertiary">
             Trang chủ
           </div>
-        </NavLink>
-        <NavLink to="/movies">
+        </div>
+        <div onClick={() => navigate("/movies")}>
           <div className="cursor-pointer category hover:text-tertiary">
             Phim
           </div>
-        </NavLink>
-        <NavLink to="/events">
+        </div>
+        <div>
           <div className="cursor-pointer category hover:text-tertiary">
             Sự kiện
           </div>
-        </NavLink>
-        <NavLink to="/contact">
+        </div>
+        <div>
           <div className="cursor-pointer category hover:text-tertiary">
             Liên hệ
           </div>
-        </NavLink>
+        </div>
       </div>
     </div>
   );
